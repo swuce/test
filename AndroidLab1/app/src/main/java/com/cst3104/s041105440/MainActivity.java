4265b55e6eb4f23d497fb974e95eb1d3d23cd6d2
Viktor Veljovic <vik.veljovic@gmail.com>
2024-09-27 11:54:31 -0400

    Initial commit

---
+++ AndroidLab1/app/src/main/java/com/cst3104/s041105440/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cst3104.s041105440;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
